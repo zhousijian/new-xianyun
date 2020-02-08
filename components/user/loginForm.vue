00076a5ea8db420c254860e660e67ee94abe333b
zhousijian <13553593242@163.com>
2020-02-08 16:28:22 +0800

    完成登录页面的组件引入和登录form表单的组件静态结构

---
+++ components/user/loginForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-form :model="userInfo" :rules="rules" ref="ruleForm" class="form">
+    <el-form-item prop="username">
+      <el-input v-model="userInfo.username" placeholder="用户名/手机"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input v-model="userInfo.password" type="password" placeholder="密码"></el-input>
+    </el-form-item>
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
+    <el-form-item>
+      <el-button class="submit" type="primary" @click="submitForm">登录</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        username: "13800138000",
+        password: "123456"
+      },
+      rules: {
+        username: [{ required: true, message: "请输入账号", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(this.userInfo);
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
