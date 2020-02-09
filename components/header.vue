b62514e99164118bbb77fbb299e93897e58b7e93
zhousijian <13553593242@163.com>
2020-02-09 17:08:34 +0800

    点击退出，清楚本地存储

--- components/header.vue
+++ components/header.vue
@@ -23,7 +23,12 @@
             </el-row>
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item>个人中心</el-dropdown-item>
-              <el-dropdown-item>退出</el-dropdown-item>
+              <!-- el-dropdown-item是elementui的组件，它不接受click事件 -->
+              <!-- @click.native是可以的，native会把事件加在组件在外层的div -->
+              <el-dropdown-item @click.native='logout'>退出
+                <!-- 要么直接加上一个div也可以 -->
+                <!-- <div @click="logout">退出</div> -->
+              </el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
           <nuxt-link to="/user/login" v-else>登录 / 注册</nuxt-link>
@@ -44,6 +49,12 @@ export default {
         { text: "国内机票", url: "/air" }
       ],
     };
+  },
+  methods: {
+    logout(){
+      // console.log(111);
+      this.$store.commit('user/setUserInfo',{})
+    }
   }
 };
 </script>
