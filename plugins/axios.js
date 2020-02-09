fbd6a0ea19430b6adf4047981547a46800b4065d
zhousijian <13553593242@163.com>
2020-02-09 22:54:51 +0800

    添加axios的拦截插件
    使用错误拦截

---
+++ plugins/axios.js
@@ -0,0 +1,20 @@
+// 这文件名不是固定axios，只是文件时axios拦截器
+
+import { Message } from 'element-ui'
+
+// 网页启动时候执行该文件，data参数时插件里面本来就有的
+export default (data)=>{
+    // console.log('有没有经过这部？axios插件');
+    // console.log(data);
+    // 拦截请求的错误
+    data.$axios.onError(err=>{
+        // console.log(err.response);
+        const { statusCode,message} = err.response.data
+        // 判断状态是否为400，展示错误信息
+        if(statusCode == 400){
+            // 使用element-ui组件提示错误
+            Message.error(message)
+        }
+        
+    })
+}
