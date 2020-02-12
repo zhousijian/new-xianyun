67408b971a56163b438804b815f0680471b66d9b
zhousijian <13553593242@163.com>
2020-02-12 17:43:27 +0800

    完成机票详情页的航班头部和航班信息静态结构，并且渲染航班信息

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="flights">
-      航班详情页
-  </div>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <!-- <div></div> -->
+        <FlightsHead></FlightsHead>
+
+        <!-- 航班信息 -->
+        <!-- <div></div> -->
+        <FlightsItem v-for="(item,index) in searchFlights" :key="index" :data='item'></FlightsItem>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-export default {
+// import moment from "moment";
+import FlightsHead from '@/components/air/flightsHead'
+import FlightsItem from '@/components/air/flightsItem'
 
-}
+export default {
+  data() {
+    return {
+      searchFlights : {}
+    };
+  },
+  components: {
+    FlightsHead,FlightsItem
+  },
+  mounted () {
+    // console.log(this.$route.query);
+    this.$axios({
+      url : '/airs',
+      params : this.$route.query
+    }).then(res=>{
+      // console.log(res);
+      this.searchFlights = res.data.flights
+    })
+  },
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
+
+.aside {
+  width: 240px;
+}
 </style>
