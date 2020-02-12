dc553f4166a31a4cf235c044a7cb0b0729ae36c0
zhousijian <13553593242@163.com>
2020-02-12 19:32:21 +0800

    完成机票首页里的特价机票部分

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -38,16 +38,44 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale"></div>
+    <div class="air-sale">
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <nuxt-link
+            :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+          >
+            <img :src="item.cover" />
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{item.departCity}}-{{item.destCity}}</span>
+              <span>￥{{item.price}}</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </div>
   </section>
 </template>
 
 <script>
 // 引入机票首页的form搜索框组件
-import IndexSearchForm from '@/components/air/indexSearchForm'
+import IndexSearchForm from "@/components/air/indexSearchForm";
 export default {
+  data () {
+    return {
+      sales : []
+    }
+  },
   components: {
     IndexSearchForm
+  },
+  mounted () {
+    this.$axios({
+      url : '/airs/sale'
+    }).then(res=>{
+      // console.log(res);
+      this.sales = res.data.data
+      
+    })
   }
 };
 </script>
