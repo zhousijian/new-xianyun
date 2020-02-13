495f69c3f26cf8affae4a2eec04bec823cda88e3
zhousijian <13553593242@163.com>
2020-02-13 20:32:52 +0800

    完成撤销功能

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -87,89 +87,96 @@ export default {
         // 找出不符合条件
 
         // 起飞机场
-        if(this.airport != '' && this.airport != v.org_airport_name){
-            valid = false
+        if (this.airport != "" && this.airport != v.org_airport_name) {
+          valid = false;
         }
         // 航班公司
-        if(this.company != '' && this.company != v.airline_name){
-            valid = false
+        if (this.company != "" && this.company != v.airline_name) {
+          valid = false;
         }
         //出发时间
         // console.log(this.flightTimes);
         // 时间范围
-        let scopedHour = this.flightTimes.split('-')
+        let scopedHour = this.flightTimes.split("-");
         // 开始时间的时位
-        let startHour = v.dep_time.split(':')[0]
-        if(Number(scopedHour[0]) > Number(startHour) || Number(startHour) >= Number(scopedHour[1])){
-            valid = false
+        let startHour = v.dep_time.split(":")[0];
+        if (
+          Number(scopedHour[0]) > Number(startHour) ||
+          Number(startHour) >= Number(scopedHour[1])
+        ) {
+          valid = false;
         }
         // 机型大小
-        if(this.airSize != '' && this.airSize != v.plane_size){
-            valid = false
+        if (this.airSize != "" && this.airSize != v.plane_size) {
+          valid = false;
         }
         return valid;
       });
       this.$emit("filterDate", arr);
-      return '';
+      return "";
     }
   },
   methods: {
     // 选择机场时候触发
     handleAirport(value) {
-    //   // filter筛选出满足条件的返回一个新的数组
-    //   let arr = this.data.flights.filter(v => {
-    //     return v.org_airport_name === value;
-    //   });
-    //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
-    //   this.$emit("filterDate", arr);
-    //   // console.log(arr);
+      //   // filter筛选出满足条件的返回一个新的数组
+      //   let arr = this.data.flights.filter(v => {
+      //     return v.org_airport_name === value;
+      //   });
+      //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
+      //   this.$emit("filterDate", arr);
+      //   // console.log(arr);
     },
 
     // 选择出发时间时候触发
     handleFlightTimes(value) {
-    //   //   console.log(value);
-    //   let arr = this.data.flights.filter(v => {
-    //     // 时间范围
-    //     let scopeHour = value.split("-");
-    //     // console.log(scopeHour);
-    //     // 开始时间
-    //     let startHour = v.dep_time.split(":")[0];
-    //     // console.log(Number(scopeHour[1]));
-    //     return (
-    //       Number(scopeHour[0]) <= Number(startHour) &&
-    //       Number(startHour) < Number(scopeHour[1])
-    //     );
-    //   });
-    //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
-    //   this.$emit("filterDate", arr);
+      //   //   console.log(value);
+      //   let arr = this.data.flights.filter(v => {
+      //     // 时间范围
+      //     let scopeHour = value.split("-");
+      //     // console.log(scopeHour);
+      //     // 开始时间
+      //     let startHour = v.dep_time.split(":")[0];
+      //     // console.log(Number(scopeHour[1]));
+      //     return (
+      //       Number(scopeHour[0]) <= Number(startHour) &&
+      //       Number(startHour) < Number(scopeHour[1])
+      //     );
+      //   });
+      //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
+      //   this.$emit("filterDate", arr);
     },
 
     // 选择航空公司时候触发
     handleCompany(value) {
-    //   // console.log(value);
-
-    //   let arr = this.data.flights.filter(v => {
-    //     return v.airline_name == value;
-    //   });
-    //   // console.log(arr);
-    //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
-    //   this.$emit("filterDate", arr);
+      //   // console.log(value);
+      //   let arr = this.data.flights.filter(v => {
+      //     return v.airline_name == value;
+      //   });
+      //   // console.log(arr);
+      //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
+      //   this.$emit("filterDate", arr);
     },
 
     // 选择机型时候触发
     handleAirSize(value) {
-    //   // console.log(value);
-    //   // filter筛选出满足条件的返回一个新的数组
-    //   let arr = this.data.flights.filter(v => {
-    //     return v.plane_size === value;
-    //   });
-    //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
-    //   this.$emit("filterDate", arr);
-    //   // console.log(arr);
+      //   // console.log(value);
+      //   // filter筛选出满足条件的返回一个新的数组
+      //   let arr = this.data.flights.filter(v => {
+      //     return v.plane_size === value;
+      //   });
+      //   // 子传父，把筛选满足条件的新数组传给flights.vue组件，渲染
+      //   this.$emit("filterDate", arr);
+      //   // console.log(arr);
     },
 
     // 撤销条件时候触发
-    handleFiltersCancel() {}
+    handleFiltersCancel() {
+      this.airport = ""; // 机场
+      this.flightTimes = ""; // 出发时间
+      this.company = ""; // 航空公司
+      this.airSize = ""; // 机型大小
+    }
   }
 };
 </script>
